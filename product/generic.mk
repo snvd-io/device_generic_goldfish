f95cfd2e3d035292d9ca741059e49f86c0e2db84
Roman Kiryanov <rkir@google.com>
2024-08-20 21:17:43 +0000

    Move phone overlays into a separate file
    to be used in phone.mk and gphone.mk. Other
    target should not use phone specific things.
    
    Bug: 361152997
    Test: presubmit
    Change-Id: I23859efb2931f0c23d95dfb707fb848ee9936c4c
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- product/generic.mk
+++ product/generic.mk
@@ -162,46 +162,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
-PRODUCT_PACKAGES += \
-    DisplayCutoutEmulationEmu01Overlay \
-    EmulationPixelFoldOverlay \
-    SystemUIEmulationPixelFoldOverlay \
-    EmulationPixel8ProOverlay \
-    SystemUIEmulationPixel8ProOverlay \
-    EmulationPixel8aOverlay \
-    SystemUIEmulationPixel8aOverlay \
-    EmulationPixel8Overlay \
-    SystemUIEmulationPixel8Overlay \
-    EmulationPixel7ProOverlay \
-    SystemUIEmulationPixel7ProOverlay \
-    EmulationPixel7Overlay \
-    SystemUIEmulationPixel7Overlay \
-    EmulationPixel7aOverlay \
-    SystemUIEmulationPixel7aOverlay \
-    EmulationPixel6ProOverlay \
-    SystemUIEmulationPixel6ProOverlay \
-    EmulationPixel6Overlay \
-    SystemUIEmulationPixel6Overlay \
-    EmulationPixel6aOverlay \
-    SystemUIEmulationPixel6aOverlay \
-    EmulationPixel5Overlay \
-    SystemUIEmulationPixel5Overlay \
-    EmulationPixel4XLOverlay \
-    SystemUIEmulationPixel4XLOverlay \
-    EmulationPixel4Overlay \
-    SystemUIEmulationPixel4Overlay \
-    EmulationPixel4aOverlay \
-    SystemUIEmulationPixel4aOverlay \
-    EmulationPixel3XLOverlay \
-    SystemUIEmulationPixel3XLOverlay \
-    EmulationPixel3Overlay \
-    SystemUIEmulationPixel3Overlay \
-    EmulationPixel3aOverlay \
-    SystemUIEmulationPixel3aOverlay \
-    EmulationPixel3aXLOverlay \
-    SystemUIEmulationPixel3aXLOverlay \
-    EmulationPixel2XLOverlay \
-    NavigationBarMode2ButtonOverlay \
+# b/361152997: move to the phone specific place
+$(call inherit-product, device/generic/goldfish/product/phone_overlays.mk)
 
 # Enable Uwb
 PRODUCT_PACKAGES += \
